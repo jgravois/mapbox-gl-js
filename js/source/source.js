0694cbc5e8c2883be66b1a875550a1d30ded7dab
chelm <christopher.helm@gmail.com>
2015-07-20 11:44:01 -0600

    better pyramid creation when using an index

--- js/source/source.js
+++ js/source/source.js
@@ -9,6 +9,24 @@ var TileCoord = require('./tile_coord');
 
 exports._loadTileJSON = function(options) {
 
+    var buildPyramid = function (index) {
+        this._pyramid = new TilePyramid({
+            index: index,
+            tileSize: this.tileSize,
+            cacheSize: 20,
+            minzoom: this.minzoom,
+            maxzoom: this.maxzoom,
+            roundZoom: this.roundZoom,
+            reparseOverscaled: this.reparseOverscaled,
+            load: this._loadTile.bind(this),
+            abort: this._abortTile.bind(this),
+            unload: this._unloadTile.bind(this),
+            add: this._addTile.bind(this),
+            remove: this._removeTile.bind(this),
+            redoPlacement: this._redoTilePlacement ? this._redoTilePlacement.bind(this) : undefined
+        });
+    }.bind(this);
+
     var loaded = function(err, tileJSON) {
         if (err) {
             this.fire('error', {error: err});
@@ -31,12 +49,12 @@ exports._loadTileJSON = function(options) {
                   return;
                 }
 
-                buildPyramid(null, index);
+                buildPyramid(index.index);
                 this.fire('load');
 
             }.bind(this));
         } else {
-            buildPyramid(null, {});
+            buildPyramid({});
             this.fire('load');
         }
 
